ad561d5521d2f71f6612a3cdd380f3ae7918705f
Rahul Shahare <rahulshahare@gmail.com>
2020-05-07 13:36:59 +0530

    index file

---
+++ precache-manifest.6637dbbad5d52c61bf9392ea54303a9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a1fd3cca2f8b70f2417",
+    "url": "/static/css/main.5520597d.chunk.css"
+  },
+  {
+    "revision": "7a1fd3cca2f8b70f2417",
+    "url": "/static/js/main.7a1fd3cc.chunk.js"
+  },
+  {
+    "revision": "e3ac90f5bbdc5275031b",
+    "url": "/static/js/1.e3ac90f5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9b3b10fca1aea94f586c7d8f4b34a1bf",
+    "url": "/index.html"
+  }
+];
